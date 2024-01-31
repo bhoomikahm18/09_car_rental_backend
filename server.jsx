6701034db7cd27d7ba424bba80a1b12298a4f291
Bhoomika <bhoomikahm18@gmail.com>
2024-01-31 13:28:48 +0530

    Connecting to database

--- server.jsx
+++ server.jsx
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express()
-const port = 5000
 
 app.get('/', (req, res) => res.send('Hello world'))
-app.listen(port, ()=>console.log(`Example App on ${port}`))
+
+mongoose.connect(`mongodb+srv://bhoomikahm18:${process.env.PASSWORD}@cluster0.276ab22.mongodb.net/cars?retryWrites=true&w=majority`)
+    .then(app.listen(process.env.PORT))
+    .then(() => console.log(`Connected to database and server is running at ${process.env.PORT}`)
+    )
+    .catch (err => console.log(err))
+
+
+//8MhI6FPlaj4LFri3
